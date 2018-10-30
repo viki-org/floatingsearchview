ab69c01b0144edd7be30410acccab4245e0286a8
John.Chang <yuchuan.chang@viki.com>
2018-10-30 13:43:02 +0800

    Migrate support library to androidx Problem upport Library 28.0.0 is last release under the android.support packaging and all bugfixs will do on androidx packaging. That means that when ads, chromecast has fixed later. we can't catch up immediately. That's why we need to migration now.
    Solution
    Solve issues which symbol cannot find after migration
    
    [#885671290193171]

--- library/src/main/java/com/arlib/floatingsearchview/suggestions/SearchSuggestionsAdapter.java
+++ library/src/main/java/com/arlib/floatingsearchview/suggestions/SearchSuggestionsAdapter.java
@@ -18,10 +18,10 @@ package com.arlib.floatingsearchview.suggestions;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
-import android.support.annotation.ColorInt;
-import android.support.annotation.DrawableRes;
-import android.support.v4.graphics.drawable.DrawableCompat;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.ColorInt;
+import androidx.annotation.DrawableRes;
+import androidx.core.graphics.drawable.DrawableCompat;
+import androidx.recyclerview.widget.RecyclerView;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
